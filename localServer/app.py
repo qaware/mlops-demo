2bda9dce01751cc8d2c83c121f205e095cbaaf45
Lars Kleinschmidt <lars.kleinschmidt@stud.h-da.de>
2023-09-01 16:47:14 +0200

    added most basic flask server

---
+++ localServer/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/run/', methods=['POST'])
+def run():
+    return "Not Implemented"
+
+
+@app.route('/status/', methods=['GET'])
+def status():
+    return "Not Implemented"
+
+
+@app.route('/predict/', methods=['POST'])
+def predict():
+    return "Not Implemented"
+
+
+@app.route('/health/')
+def health():
+    return "OK"
+
+
+def extract_gc_object_name(uri):
+    return "/".join(uri.split("/")[3:])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
