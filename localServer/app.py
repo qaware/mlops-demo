d49adca2e490e4df703b655f76113f13217f4010
Lars Kleinschmidt <lars.kleinschmidt@qaware.de>
2023-09-22 16:50:32 +0200

    predict endpoint, but with auth error for now

--- localServer/app.py
+++ localServer/app.py
@@ -4,6 +4,8 @@ from flask import Flask, request
 
 from pipeline import run_pipeline
 
+from google.cloud import aiplatform
+
 app = Flask(__name__)
 
 
@@ -24,7 +26,34 @@ def status():
 
 @app.route('/predict/', methods=['POST'])
 def predict():
-    return "Not Implemented"
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        google_cloud_project = '1053517987499'
+        google_cloud_region = 'europe-west1'
+        endpoint_id = '8983317862185697280'
+
+        # The AI Platform services require regional API endpoints.
+        client_options = {
+            'api_endpoint': google_cloud_region + '-aiplatform.googleapis.com'
+        }
+        # Initialize client that will be used to create and send requests.
+        client = aiplatform.gapic.PredictionServiceClient(client_options=client_options)
+
+        endpoint = client.endpoint_path(
+            project=google_cloud_project,
+            location=google_cloud_region,
+            endpoint=endpoint_id,
+        )
+
+        # Data
+        instances = json.loads(request.data)['instances']
+
+        # Send a prediction request and get response.
+        response = client.predict(endpoint=endpoint, instances=instances)
+
+        return response
+    else:
+        return 'Content-Type not supported!'
 
 
 @app.route('/health/')
