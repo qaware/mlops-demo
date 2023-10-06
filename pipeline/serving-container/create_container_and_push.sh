0fbad67ea7a8ae90f100bb774bd23ffee10dcf85
Martin Siehler <martin.siehler@qaware.de>
2023-10-06 15:01:00 +0200

    removed pipeline folder

--- pipeline/serving-container/create_container_and_push.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-
-docker build . -t serving-container
-docker tag serving-container europe-west1-docker.pkg.dev/ai-gilde/demo/serving-container
-docker push europe-west1-docker.pkg.dev/ai-gilde/demo/serving-container:latest
