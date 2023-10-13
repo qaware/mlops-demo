175a0c062a37130d7b1f329fbc38e60f95a61613
Lars Kleinschmidt <lars.kleinschmidt@qaware.de>
2023-10-13 09:28:02 +0200

    implemented predict call

--- frontend/src/components/PredictionContent.tsx
+++ frontend/src/components/PredictionContent.tsx
@@ -1,21 +1,47 @@
 import * as React from 'react';
 import { Box, Button, Grid, Paper, TextField, Typography } from "@mui/material";
 
+const handleSubmit = (event: React.ChangeEvent<any>) => {
+    event.preventDefault();
+    prediction(event.target[0].value.replace(/\s+/g, '').split(','));
+};
+
+const prediction = async (words: string[]) => {
+    await fetch('http://localhost:8080/predict/', {
+        method: 'POST',
+        body: JSON.stringify({
+            instances: words,
+        }),
+        headers: {
+            'Content-type': 'application/json',
+        },
+    })
+        .then((response) => response.text())
+        .then((data) => {
+            alert(data)
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+};
+
 export default function PredictionContent() {
     return (
 
         <Paper elevation={0} style={{margin: '2em', padding: '1em'}} variant='outlined'>
+            <form onSubmit={handleSubmit}>
             <Typography variant="h4" component="div" sx={{flexGrow: 1}}>
                 Prediction
             </Typography>
             <Grid item xs={6}>
-                <TextField id="label" label="Label" variant="outlined" fullWidth/>
+                <TextField id="label" label="word1, word2, word3" variant="outlined" fullWidth/>
             </Grid>
             <Grid item>
-                <Button variant="contained">
+                <Button variant="contained" type="submit">
                     Predict
                 </Button>
             </Grid>
+            </form>
             <Box mt={5}>
                 <Grid container justifyContent="flex-end">
                     <Button variant="contained" color="error">
