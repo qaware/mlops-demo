4b7ae7c050ee9e8bd1c24cf08678e528fcd4d3ad
Lars Kleinschmidt <lars.kleinschmidt@qaware.de>
2023-12-08 15:28:41 +0100

    added a basic documentation

--- frontend/src/components/DocumentationContent.tsx
+++ frontend/src/components/DocumentationContent.tsx
@@ -1,11 +1,154 @@
 import * as React from 'react';
-import { Paper } from "@mui/material";
+import {Accordion, AccordionDetails, AccordionSummary, Paper, Typography} from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+const data_gen_code =
+`from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+    
+tokenizer = Tokenizer(num_words=len(input_data))
+tokenizer.fit_on_texts(input_data)
+
+sequences = tokenizer.texts_to_sequences(input_data)
+padded_sequences = pad_sequences(sequences, padding='post')`;
+
+const train_code =
+`from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, GlobalAveragePooling1D, Dense
+    
+model = Sequential()
+model.add(Embedding(input_dim=len(labels), output_dim=16, input_length=len(padded_sequences[0])))  # Adjust input_dim and output_dim
+model.add(GlobalAveragePooling1D())
+model.add(Dense(1, activation='sigmoid'))  # Single output neuron with sigmoid for binary classification
+
+# Compile the model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])`;
+
+const deploy_code =
+`uploaded_model = aiplatform.Model.upload(
+    display_name=display_name,
+    location="europe-west1",
+    parent_model="xyz",
+    serving_container_image_uri="europe-west1-docker.pkg.dev/ai-gilde/demo/serving-container:latest",
+    artifact_uri=f'{data_path}demo-model/1',
+)
+
+endpoint = aiplatform.Endpoint.create(
+    display_name=display_name + "_endpoint",
+    project="ai-gilde",
+    location="europe-west1",
+)
+
+uploaded_model.deploy(
+    endpoint=endpoint,
+    traffic_split={"0": 100},
+    machine_type='n1-standard-2',
+)`;
+
+const verify_endpoint_code =
+    `client = aiplatform.gapic.PredictionServiceClient(client_options=client_options)
+
+endpoint = client.endpoint_path(
+    project=google_cloud_project,
+    location=google_cloud_region,
+    endpoint=endpoint_id,
+)
+
+instances = ['this is cool', 'this is bad']
+
+# Send a prediction request and get response.
+response = client.predict(endpoint=endpoint, instances=instances)`;
 
 export default function DocumentationContent() {
+
+
     return (
 
         <Paper elevation={0} style={{margin: '2em', padding: '1em', marginBottom: '5em'}} variant='outlined'>
-            Documentation ....
+            Documentation
+            <br/><br/>
+            <b>Step 1: Data Gen</b>
+            <br/>
+            The first step gathers and processes the data. In this Demo the data comes from the two input fields via an api.
+            It could also be retrieved from a DB, files or any other data source.
+            <br/><br/>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="code-content"
+                    id="code-header"
+                >
+                    <Typography>View Code</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <SyntaxHighlighter language="python" style={materialDark}>
+                        {data_gen_code}
+                    </SyntaxHighlighter>
+                </AccordionDetails>
+            </Accordion>
+            <br/><br/>
+            <b>Step 2: Train</b>
+            <br/>
+            The training step retrieves the preprocessed data from step 1 (padded_sequences and labels) and trains the model.
+            <br/><br/>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="code-content"
+                    id="code-header"
+                >
+                    <Typography>View Code</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <SyntaxHighlighter language="python" style={materialDark}>
+                        {train_code}
+                    </SyntaxHighlighter>
+                </AccordionDetails>
+            </Accordion>
+            <br/><br/>
+            <b>Step 3: Deploy</b>
+            <br/>
+            Our model gets deployed on a server. Therefore we have a serving Docker-Container,
+            which uses the model to serve an api for prediction. For this demo, we use Vertex AI Online Predicition.
+            <br/><br/>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="code-content"
+                    id="code-header"
+                >
+                    <Typography>View Code</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <SyntaxHighlighter language="python" style={materialDark}>
+                        {deploy_code}
+                    </SyntaxHighlighter>
+                </AccordionDetails>
+            </Accordion>
+            <br/><br/>
+            <b>Step 4: Verify Endpoint</b>
+            <br/>
+            Test data uses the api of our deployment to check if it is available as expected.
+            <br/><br/>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="code-content"
+                    id="code-header"
+                >
+                    <Typography>View Code</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <SyntaxHighlighter language="python" style={materialDark}>
+                        {verify_endpoint_code}
+                    </SyntaxHighlighter>
+                </AccordionDetails>
+            </Accordion>
+            <br/><br/>
+            These steps can be extended or changed as you like. For example, a normally necessary step is the
+            verifying of the model accuracy. In this demo case we don't want to do that.
         </Paper>
     );
 }
---
+++ frontend/src/react-syntax-highlighter.d.ts
@@ -0,0 +1,5 @@
+declare module 'src/react-syntax-highlighter';
+declare module 'react-syntax-highlighter/dist/esm/styles/prism';
+declare module 'react-syntax-highlighter';
+declare module 'react-syntax-highlighter/dist/cjs';
+declare module 'react-syntax-highlighter/dist/esm/styles/prism';
